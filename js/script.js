f605db8a2aa080ef62432101d7f2aa1c15935dab
Lucas Owino <lucowish35@gmail.com>
2020-03-03 14:33:18 +0300

    Add review lorem ipsum text in card body

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $(".press").click(function(){
+        $(".press").hide();
+        $(".hide1").fadeIn();
+    });
+    $(".hide1").click(function(){
+        $(".press").hide();
+        $(".press").fadeIn();
+    });
+
+    $(".press").click(function(){
+        $(".press").hide();
+        $(".hide1").fadeIn();
+    });
+    $(".hide1").click(function(){
+        $(".press").hide();
+        $(".press").fadeIn();
+    });
+
+    $(".press").click(function(){
+        $(".press").hide();
+        $(".hide1").fadeIn();
+    });
+    $(".hide1").click(function(){
+        $(".press").hide();
+        $(".press").fadeIn();
+    });
+
+    $(".press").click(function(){
+        $(".press").hide();
+        $(".hide1").fadeIn();
+    });
+    $(".hide1").click(function(){
+        $(".press").hide();
+        $(".press").fadeIn();
+    });
+
+    $(".press").click(function(){
+        $(".press").hide();
+        $(".hide1").fadeIn();
+    });
+    $(".hide1").click(function(){
+        $(".press").hide();
+        $(".press").fadeIn();
+    });
+});
