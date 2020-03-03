87c76d8451f5dd2b57de23670e6d5a7e59e9f4af
Lucas Owino <lucowish35@gmail.com>
2020-03-03 14:48:58 +0300

    Add toggle function in java script

--- js/script.js
+++ js/script.js
@@ -1,46 +1,55 @@
 $(document).ready(function(){
-    $(".press").click(function(){
-        $(".press").hide();
+    $("#press").click(function(){
+        $("#press").hide();
         $(".hide1").fadeIn();
     });
     $(".hide1").click(function(){
-        $(".press").hide();
-        $(".press").fadeIn();
+        $(".hide1").hide();
+        $("#press").fadeIn();
     });
 
-    $(".press").click(function(){
-        $(".press").hide();
-        $(".hide1").fadeIn();
+    $(".#press1").click(function(){
+        $("#press1").hide();
+        $(".hide2").fadeIn();
     });
-    $(".hide1").click(function(){
-        $(".press").hide();
-        $(".press").fadeIn();
-    });
-
-    $(".press").click(function(){
-        $(".press").hide();
-        $(".hide1").fadeIn();
-    });
-    $(".hide1").click(function(){
-        $(".press").hide();
-        $(".press").fadeIn();
+    $(".hide2").click(function(){
+        $(".hide2").hide();
+        $("#press1").fadeIn();
     });
 
-    $(".press").click(function(){
-        $(".press").hide();
-        $(".hide1").fadeIn();
+    $("#press2").click(function(){
+        $("#press2").hide();
+        $(".hide3").fadeIn();
     });
-    $(".hide1").click(function(){
-        $(".press").hide();
-        $(".press").fadeIn();
+    $(".hide3").click(function(){
+        $(".hide3").hide();
+        $("#press2").fadeIn();
     });
 
-    $(".press").click(function(){
-        $(".press").hide();
-        $(".hide1").fadeIn();
+    $("#press3").click(function(){
+        $("#press3").hide();
+        $(".hide4").fadeIn();
     });
-    $(".hide1").click(function(){
-        $(".press").hide();
-        $(".press").fadeIn();
+    $(".hide4").click(function(){
+        $("#press3").hide();
+        $("#press3").fadeIn();
+    });
+
+    $("#press4").click(function(){
+        $("#press4").hide();
+        $(".hide5").fadeIn();
+    });
+    $(".hide5").click(function(){
+        $("#press4").hide();
+        $("#press4").fadeIn();
+    });
+
+    $("#press5").click(function(){
+        $("#press5").hide();
+        $(".hide6").fadeIn();
+    });
+    $(".hide6").click(function(){
+        $("#press5").hide();
+        $("#press5").fadeIn();
     });
 });
